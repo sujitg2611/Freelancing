a347a37de3f417a1e8297f0b50ec43e8c4212837
jitu712 <jitu712@gmail.com>
2023-10-02 15:18:49 +0530

    final frontend

--- GigGenius/src/pages/myGigs/MyGigs.jsx
+++ GigGenius/src/pages/myGigs/MyGigs.jsx
@@ -1,7 +1,95 @@
 import React from 'react';
 import './MyGigs.scss';
+import { Link } from 'react-router-dom';
+
 const MyGigs = () => {
-  return <div>MyGigs</div>;
+  return (
+    <div className="myGigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add">
+            <button>Add new gig</button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyGigs;
--- GigGenius/src/pages/orders/Orders.jsx
+++ GigGenius/src/pages/orders/Orders.jsx
@@ -1,7 +1,97 @@
 import React from 'react';
 import './Orders.scss';
+import { Link } from 'react-router-dom';
+
 const Orders = () => {
-  return <div>Orders</div>;
+  const currentUser = {
+    id: 1,
+    username: 'Sujit Gouda',
+    isSeller: true,
+  };
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? 'Buyer' : 'Seller'}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
